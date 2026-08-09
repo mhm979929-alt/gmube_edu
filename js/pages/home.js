caebe32935cd411345bb8e4eb9f15855f8debdc2
mhm979929-alt <mhm979929@gmail.com>
2026-08-09 17:22:53 +0300

    Update home.js

--- js/pages/home.js
+++ js/pages/home.js
@@ -13,7 +13,7 @@ async function renderHome() {
         </div>
         <div class="top-actions">
           ${session
-            ? `<a href="#" class="icon-btn" title="الدراسة الجماعية " style="text-decoration:none;display:inline-flex;align-items:center;justify-content:center;">
+            ? `<a href="https://live-red-zeta.vercel.app/" class="icon-btn" title="الدراسة الجماعية " style="text-decoration:none;display:inline-flex;align-items:center;justify-content:center;">
                <i data-feather="clipboard"></i>
                </a>
                <button class="icon-btn" onclick="navigateTo('/notifications')" title="الإشعارات">
